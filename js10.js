f281cd4b6c5503c2b7f5653d3739dd0dd696e8ce
Dmitriy <fstpain@gmail.com>
2024-06-16 21:00:03 +0300

    exe

--- js10.js
+++ js10.js
@@ -1,1 +1,12 @@
-console.log('12112')
+const delay = (ms)=>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve()
+        }, ms)
+    })
+
+}
+
+delay(2000).then(()=>{
+    alert('hello')
+})

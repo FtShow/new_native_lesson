3d6b0d718383920dd3320cca4266ea5771d8407e
Dmitriy <fstpain@gmail.com>
2024-06-21 23:36:43 +0300

    js11

---
+++ js11.js
@@ -0,0 +1,9 @@
+const server = {
+    getServer (){
+        return new Promise((resolve, reject)=>{
+            setTimeout(()=>{
+                resolve('PROMISE RESOLVE')
+            }, 1000)
+        })
+    }
+}

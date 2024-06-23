c3f27feb8bee7b2d2206ef61ae9fe25ad21828cc
Dmitriy <fstpain@gmail.com>
2024-06-23 11:48:00 +0300

    js9

--- js11.js
+++ js11.js
@@ -1,3 +1,5 @@
+// js11.js
+
 const server = {
     getServer() {
         return new Promise((resolve, reject) => {
@@ -7,3 +9,20 @@ const server = {
         })
     }
 }
+
+// Выполняем запрос с использованием встроенной функции fetch
+fetch('https://bing.com/query=js')
+    .then((res) => {
+        if (!res.ok) {
+            throw new Error('Network response was not ok ' + res.statusText);
+        }
+        return res.text();  // преобразуем ответ в текст для отображения в консоли
+    })
+    .then((text) => {
+        console.log(text);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+console.log(crypto.randomUUID())
---
+++ js9.js
@@ -0,0 +1,7 @@
+let car;
+car = 'bmw'
+function startEngine () {
+    console.log(`start ${car}`)
+}
+car = 'opel'
+startEngine()

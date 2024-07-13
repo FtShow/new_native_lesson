4d0c620028a4b5883b217cf45e57a6a7c106d735
Dmitriy <fstpain@gmail.com>
2024-07-13 23:18:40 +0300

    nativeJS

--- js14.js
+++ js14.js
@@ -26,14 +26,17 @@ let car2 = new ClassCar('tesla', 300)
 
 //console.log(arr.__proto__ === arr2.__proto__)
 
-let str = new String('str')
+
 let st2r = 'str'
 let n = 32
 
-console.log(st2r)
+//console.log(st2r)
 
-let a = st2r.__proto__.constructor('2222')
-console.log(car1.__proto__.__proto__ === null)
+let a = st2r.__proto__.constructor
+console.log(car1.__proto__.constructor === ClassCar.prototype)
+console.dir(Function.prototype.__proto__ === Object.prototype)
+console.dir( Object.prototype.__proto__)
+// console.log(car1.__proto__.__proto__ === null)
 //console.log(Array.from(str).join('') === st2r)
 
 
